c052e800adba5ca77acf0600189059acd00ba1f1
brett.odgers-jewell <brett.odgers-jewell@konjur.sg>
2024-03-14 10:40:55 +0800

    updetd import string

--- nifi-webscraper-processors/src/test/java/nar/WebScraperTest.java
+++ nifi-webscraper-processors/src/test/java/nar/WebScraperTest.java
@@ -19,8 +19,8 @@ package webscrape.konjur.nifi.processors.webscraper;
 import org.apache.nifi.util.MockFlowFile;
 import org.apache.nifi.util.TestRunner;
 import org.apache.nifi.util.TestRunners;
-import org.junit.jupiter.Before;
-import org.junit.jupiter.Test;
+import org.junit.jupiter.api.Before;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
